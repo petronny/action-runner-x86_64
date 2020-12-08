11d154d03dfb9de1aa20cc399aebeed85625e3b0
Jingbei Li <i@jingbei.li>
2020-12-08 23:14:25 +0800

    upgraded add-path to GITHUB_PATH

--- initialize.sh
+++ initialize.sh
@@ -19,8 +19,8 @@ pkgbase=$(jq -r .action "${GITHUB_EVENT_PATH}")
 uuid=$(echo $pkgbase | cut -d' ' -f2)
 pkgbase=$(echo $pkgbase | cut -d' ' -f1)
 
-echo "::add-path::${GITHUB_WORKSPACE}/action-tools"
-echo "::add-path::${GITHUB_WORKSPACE}/lilac"
+echo "${GITHUB_WORKSPACE}/action-tools" >> $GITHUB_PATH
+echo "${GITHUB_WORKSPACE}/lilac" >> $GITHUB_PATH
 echo "::set-output name=pkgbase::${pkgbase}"
 echo "::set-output name=uuid::${uuid}"
 echo "::set-output name=home::${HOME}"
